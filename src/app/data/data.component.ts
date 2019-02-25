7d5259fc0261aa1dcdd7a337eff600a8a45c72c3
victorjaquez <victor@jaquez.me>
2019-02-25 15:30:28 -0700

    data component logic, styling

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tweet } from './types';
+import { map, tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs/Subscription'
+import { TweetsService } from '../services/tweets.service'
 
 @Component({
   selector: 'app-data',
@@ -6,8 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.component.css']
 })
 export class DataComponent implements OnInit {
+  tweets: Tweet[] = [];
+  searchText: string = '';
+  subscriptions: Subscription[] = [];
+ 
+  executeSearch() {
+    if(!this.searchText) return;
+    const querySubscription = this.tweetService.watchQuerySearchTweets(this.searchText)
+    .valueChanges.subscribe((response) => {
+      this.tweets = response.data['twitter'].search;
+    });
 
-  constructor() { }
+    this.subscriptions = [...this.subscriptions, querySubscription]
+  }
+
+  constructor(private tweetService: TweetsService) { }
 
   ngOnInit() {
   }
