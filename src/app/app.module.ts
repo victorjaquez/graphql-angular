ba645132bbc6b35fb4f73ddb60e8a13897598160
victorjaquez <victor@jaquez.me>
2019-02-25 12:08:50 -0700

    apollo, graphql install fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GraphQLModule } from './graphql.module';
+//Used because Apollo HttpLink requires HttpClient
 import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
+
+// Service to connect our client to external GraphQL Server
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+
+// Place to store data in. Default cache implementation for Apollo. 
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ExchangeRatesComponent } from './exchange-rates/exchange-rates.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExchangeRatesComponent
   ],
   imports: [
     BrowserModule,
-    GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory(httpLink: HttpLink) {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: "https://o5x5jzoo7z.sse.codesandbox.io/graphql"
+        })
+      }
+    },
+    deps: [HttpLink]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
